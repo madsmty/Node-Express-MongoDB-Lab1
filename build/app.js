7a01f39afa13531c516b4b550fe5b6adea3fcb47
miguel_deleon_acn <miguel.angel.de.leon@accenture.com>
2023-05-03 11:36:42 -0600

    Axios calls

--- build/app.js
+++ build/app.js
@@ -1,13 +1,39 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const axios_1 = __importDefault(require("axios"));
 const app = (0, express_1.default)();
 const port = 3000;
+function callExtAPI(url) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield axios_1.default.get(url);
+            console.log(response);
+            return response;
+        }
+        catch (error) {
+            console.log(error);
+            throw new Error(`Error in 'callExtAPI(${url})': ${error}`);
+        }
+    });
+}
+;
 function checkJWT(req, res, next) {
     console.log("Check JWT");
+    console.log("- If JWT valid: next()");
+    console.log("- else, response status = 401");
     next();
 }
 ;
@@ -21,12 +47,24 @@ app.use(logToDB);
 app
     .route('/users')
     .get((req, res) => {
-    return res.send("Response with list of users");
+    const url = "https://jsonplaceholder.typicode.com/users/";
+    callExtAPI(url);
+    console.log('GET /users');
+    console.log('- Transform result into desired format');
+    console.log('- Send response with transformed info');
+    return res.sendStatus(200);
 });
 app
     .route('/users/:userId/posts')
     .get((req, res) => {
-    return res.send(`Posts created by user ${req.params.userId}`);
+    const url = `https://jsonplaceholder.typicode.com/users/${req.params.userId}/posts`;
+    console.log(url);
+    callExtAPI(url);
+    console.log('get /users/:userId/posts');
+    console.log('- Filter result for specified UserId');
+    console.log('- Transform result into desired format');
+    console.log('- Send response with transformed info');
+    return res.sendStatus(200);
 });
 app.listen(port, () => {
     console.log(`Connected succesfully in port ${port}`);
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
 import express, {Application, NextFunction, Request, Response} from "express";
+import axios, {AxiosResponse} from "axios";
 const app: Application = express();
 const port: number = 3000;
 
 
+async function callExtAPI<T>(url:string): Promise<AxiosResponse> {
+   try {
+        const response = await axios.get(url);
+        console.log(response);
+        return response;
+    } catch (error) {
+        console.log(error);
+        throw new Error(`Error in 'callExtAPI(${url})': ${error}`);
+    }
+  }; 
 
 function checkJWT(req: Request, res: Response, next: NextFunction) {
     console.log("Check JWT"); 
-    console.log("If JWT valid: next()");
-    console.log("else, response status = 401")
+    console.log("- If JWT valid: next()");
+    console.log("- else, response status = 401")
     next();
  };
 
@@ -22,19 +33,24 @@ app.use(logToDB);
 app
 .route('/users')
 .get((req: Request, res: Response) => {
-    console.log('Use AXIOS to call API');
-    console.log('Transform result into desired format');
-    console.log('Send response with transformed info');
+    const url:string = "https://jsonplaceholder.typicode.com/users/";
+    callExtAPI(url);
+    console.log('GET /users');
+    console.log('- Transform result into desired format');
+    console.log('- Send response with transformed info');
     return res.sendStatus(200);
 });
  
 app
 .route('/users/:userId/posts')
 .get((req: Request, res: Response) => {
-    console.log('Use AXIOS to call API');
-    console.log('Filter result for specified UserId');
-    console.log('Transform result into desired format');
-    console.log('Send response with transformed info');
+    const url:string = `https://jsonplaceholder.typicode.com/users/${req.params.userId}/posts`;
+    console.log(url);
+    callExtAPI(url);
+    console.log('get /users/:userId/posts');
+    console.log('- Filter result for specified UserId');
+    console.log('- Transform result into desired format');
+    console.log('- Send response with transformed info');
     return res.sendStatus(200);
 });
 
