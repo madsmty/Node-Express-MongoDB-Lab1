21c59a2dad6f075d991b0d86b6f7e43ddc04b940
Valadez Martinez <d.valadez.martinez@accenture.com>
2023-05-09 14:28:20 -0600

    Deleting node modules

--- build/models/logEntry.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const logEntry = {
-    "resourceAccessed": "REST path accessed",
-    "userId": "",
-    "dateCreated": "current Datetime"
-};
--- build/models/posts.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const posts = {
-    "userId": "",
-    "name": "firstName + lastName",
-    "email": "userEmail",
-    "postId": "postId",
-    "title": "",
-    "body": ""
-};
