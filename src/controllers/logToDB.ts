40af876d48ede6dd11d9cd119b98459253f6fb7c
Miguel De Leon <miguel.angel.de.leon@accenture.com>
2023-05-29 10:34:35 -0600

    Correccion de error en LogToDB

--- src/controllers/logToDB.ts
+++ src/controllers/logToDB.ts
@@ -7,6 +7,7 @@ export class LogController {
     res: Response
     req: Request
     next: NextFunction
+    
 
     constructor(req: Request, res: Response, next: NextFunction) {
         this.res = res
@@ -14,11 +15,20 @@ export class LogController {
         this.next = next
     }
 
+    
+
     async logToDB() {
+        let decoded:TokenInterface;
         const now = new Date()
         const token: string =
             this.req.headers.authorization?.split(' ')[1] || ''
-        const decoded = jwt.verify(token, 'SECRET') as TokenInterface //cambiar secret a .env
+            if (token !== '')
+{
+     decoded = jwt.verify(token, 'SECRET') as TokenInterface //cambiar secret a .env
+}        else
+{
+     decoded = {userRole:"Guest",userId:"NoUserId"}
+}
 
         const logEntry = new LogEntry({
             resourceAccessed: this.req.url,
